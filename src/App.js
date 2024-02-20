c4053e247406e5c67d65f3ad3620f348b1c985f9
mina-atallah <minaatallah18@gmail.com>
2024-02-20 11:37:21 +0200

    Handling Different App Status - made three components for different status of the app (loading - error - ready) and rendered them conditionally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -23,7 +26,10 @@ function reducer(state, action) {
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  // deprived state
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -37,8 +43,9 @@ function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,9 @@
+export default function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz!</h2>
+      <h3>{numQuestions} Questions to test your React mastery</h3>
+      <button className="btn btn-ui">let's start!</button>
+    </div>
+  );
+}
