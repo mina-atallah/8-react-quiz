f8bd1414ab1ebb8dc5f25b6aafe3dba3a8a4a027
mina-atallah <minaatallah18@gmail.com>
2024-02-20 11:58:33 +0200

    Display First Question on Click - made new component for question[s] - made new action type so we can set a new status - passed the dispatch func as prop to set/update status by clicking on button

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,14 @@
-export default function StartScreen({ numQuestions }) {
+export default function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
       <h3>{numQuestions} Questions to test your React mastery</h3>
-      <button className="btn btn-ui">let's start!</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        let's start!
+      </button>
     </div>
   );
 }
